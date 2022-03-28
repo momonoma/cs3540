aa3150b8ccdfbd5e7f549060848c2f9a56c3ae9c
chensta98 <17echen@gmail.com>
2022-03-28 15:31:50 -0400

    Updated enemy AI

--- LevelPrototypeFP2/Assets/EthansObjects/Scripts/EnemyNinjaBehavior.cs
+++ LevelPrototypeFP2/Assets/EthansObjects/Scripts/EnemyNinjaBehavior.cs
@@ -11,6 +11,10 @@ public class EnemyNinjaBehavior : MonoBehaviour
     public float attackRate = 2f;
     public Animator anim;
     public GameObject gunTip;
+    public float bulletSpeed = 20f;
+
+    float distanceToPlayer;
+    float elapsedTime = 0f;
 
     public enum EnemyState
     {
@@ -40,7 +44,9 @@ public class EnemyNinjaBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch(currentState)
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        switch (currentState)
         {
             case EnemyState.Idle:
                 UpdateIdleState();
@@ -55,26 +61,78 @@ public class EnemyNinjaBehavior : MonoBehaviour
                 UpdateMoveState();
                 break;
         }
+
+        elapsedTime += Time.deltaTime;
     }
 
     void UpdateIdleState()
     {
         anim.SetInteger("animState", 0);
+        if(distanceToPlayer <= alertDistance)
+        {
+            currentState = EnemyState.Aimed;
+        }
     }
 
     void UpdateAimedState()
     {
         anim.SetInteger("animState", 1);
+        FaceTarget(player.transform.position);
+
+        if(distanceToPlayer <= attackDistance)
+        {
+            currentState = EnemyState.Attack;
+        }
+        else if(distanceToPlayer > alertDistance)
+        {
+            currentState = EnemyState.Idle;
+        }
     }
 
     void UpdateAttackState()
     {
         anim.SetInteger("animState", 2);
+        FaceTarget(player.transform.position);
+
+        EnemyAttack();
+
+        if(distanceToPlayer > attackDistance)
+        {
+            currentState = EnemyState.Aimed;
+        }
+        else if(distanceToPlayer > alertDistance)
+        {
+            currentState = EnemyState.Idle;
+        }
     }
 
     void UpdateMoveState()
     {
         anim.SetInteger("animState", 3);
+    }
+
+    void FaceTarget(Vector3 target)
+    {
+        Vector3 directionToTarget = (target - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(directionToTarget);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 10 * Time.deltaTime);
+    }
+
+    void EnemyAttack()
+    {
+        if(elapsedTime >= attackRate)
+        {
+            var animDuration = anim.GetCurrentAnimatorStateInfo(0).length;
+            Invoke("Shoot", animDuration);
+            elapsedTime = 0.0f;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, gunTip.transform.position, gunTip.transform.rotation);
+        var _rb = bullet.GetComponent<Rigidbody>();
+        _rb.AddForce(transform.forward * bulletSpeed, ForceMode.VelocityChange);
     }
 
     private void OnDrawGizmos()
