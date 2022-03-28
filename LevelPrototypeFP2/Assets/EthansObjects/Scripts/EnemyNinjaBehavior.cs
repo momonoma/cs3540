731e044d4463675d6f5d4ed80572750b38c0ed16
chensta98 <17echen@gmail.com>
2022-03-28 14:13:27 -0400

    continued enemy work

--- LevelPrototypeFP2/Assets/EthansObjects/Scripts/EnemyNinjaBehavior.cs
+++ LevelPrototypeFP2/Assets/EthansObjects/Scripts/EnemyNinjaBehavior.cs
@@ -9,7 +9,18 @@ public class EnemyNinjaBehavior : MonoBehaviour
     public float attackDistance = 10f;
     public float alertDistance = 20f;
     public float attackRate = 2f;
+    public Animator anim;
+    public GameObject gunTip;
 
+    public enum EnemyState
+    {
+        Idle,
+        Aimed,
+        Attack,
+        Move
+    }
+
+    public EnemyState currentState;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +29,60 @@ public class EnemyNinjaBehavior : MonoBehaviour
         {
             player = GameObject.FindGameObjectWithTag("Player");
         }
+
+        anim = GetComponent<Animator>();
+
+        currentState = EnemyState.Idle;
+
+        gunTip = GameObject.Find("GunTip");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(currentState)
+        {
+            case EnemyState.Idle:
+                UpdateIdleState();
+                break;
+            case EnemyState.Aimed:
+                UpdateAimedState();
+                break;
+            case EnemyState.Attack:
+                UpdateAttackState();
+                break;
+            case EnemyState.Move:
+                UpdateMoveState();
+                break;
+        }
+    }
+
+    void UpdateIdleState()
+    {
+        anim.SetInteger("animState", 0);
+    }
+
+    void UpdateAimedState()
+    {
+        anim.SetInteger("animState", 1);
+    }
+
+    void UpdateAttackState()
+    {
+        anim.SetInteger("animState", 2);
+    }
+
+    void UpdateMoveState()
+    {
+        anim.SetInteger("animState", 3);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, alertDistance);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
     }
 }
